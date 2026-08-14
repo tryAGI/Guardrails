5539b853ce2899ff141aa943170b0856a900c7e7
Konstantin S. <havendv@gmail.com>
2026-08-14 19:44:45 +0400

    feat: Updated OpenAPI spec (#35)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Guardrails/Generated/Guardrails.GuardClient.g.cs
+++ src/libs/Guardrails/Generated/Guardrails.GuardClient.g.cs
@@ -33,10 +33,17 @@ namespace Guardrails
 
         /// <inheritdoc/>
         public global::Guardrails.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Guardrails.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Guardrails.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
